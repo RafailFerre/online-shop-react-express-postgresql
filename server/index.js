3bd08fab93c7c9a25ecc8ea1c23be362d42fa1aa
Rafail Ferre <qa.rafailferre@gmail.com>
2025-04-21 15:47:14 -0400

    update files

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 
 import { sequelize } from './db.js';
-import { User, Basket, BasketDevice, Device, Type, Brand } from './models/models.js';
+import { User, Basket, BasketDevice, Device, Type, Brand, DeviceInfo, Rating, TypeBrand } from './models/models.js';
 
 const app = express();
 
@@ -13,7 +13,7 @@ const start = async () => {
         await sequelize.authenticate(); // database connection
         console.log('Connection has been established successfully.');
         await sequelize.sync(); // creates database tables 
-        console.log('Tables have been created successfully.');
+        // console.log('Tables have been created successfully.');
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     } catch (error) {
         console.log(error);
