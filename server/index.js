77d57b5a3ab02d5ad83160bdec2ba41148647641
Rafail Ferre <qa.rafailferre@gmail.com>
2025-04-21 16:14:56 -0400

    import cors, app.use(cors()), app.use(express.json()), create method get app.get

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 
 import { sequelize } from './db.js';
 import { User, Basket, BasketDevice, Device, Type, Brand, DeviceInfo, Rating, TypeBrand } from './models/models.js';
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors()); // make requests from client (browser)
+app.use(express.json()); // parse json
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Server is running...' });
+});
 
 const start = async () => {
     try {
