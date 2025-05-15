336ef178f98d9e50d61dd2b3530df6ed0ef18dc5
Rafail Ferre <qa.rafailferre@gmail.com>
2025-05-15 19:17:51 -0400

    Create pages: Admin.jsx, Auth.jsx, Basket.jsx, Device.jsx, Home.jsx, Order.jsx, User.jsx. Create for each page React functional component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
-    <div>
-      <h1>Online Shop</h1>
-    </div>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+     
+    // <div>
+    //   <h1>Online Shop</h1>
+    // </div>
   );
 }
 
