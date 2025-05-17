a969f2ae97e0e4de227f9ced01308937aa53074f
Rafail Ferre <qa.rafailferre@gmail.com>
2025-05-16 23:51:20 -0400

    Update App.jsx: add <BrowserRouter> <AppRouter /> </BrowserRouter>. Created navigation to page URL in browser.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./AppRouter";
 
 function App() {
--- client/src/AppRouter.jsx
+++ client/src/AppRouter.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { publicRoutes, privateRoutes } from './routes';
 import { HOME_ROUTE } from './utils/consts';
